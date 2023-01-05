21989202d1f9b0025917e202561d730af772b026
klenaers <kristof@lenaers.club>
2023-01-05 22:49:23 +0100

    Add custom dsmr

--- esphome/components/dsmr/__init__.py
+++ esphome/components/dsmr/__init__.py
@@ -82,7 +82,8 @@ async def to_code(config):
     cg.add_build_flag("-DDSMR_GAS_MBUS_ID=" + str(config[CONF_GAS_MBUS_ID]))
 
     # DSMR Parser
-    cg.add_library("glmnet/Dsmr", "0.5")
+    #cg.add_library("glmnet/Dsmr", "0.6")
+    cg.add_library("https://github.com/klenaers/arduino-dsmr", "peak-power")
 
     # Crypto
     cg.add_library("rweather/Crypto", "0.4.0")
