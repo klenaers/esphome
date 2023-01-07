ce88e15ae61a5decf05bcbd4258abaf4281e9ad9
klenaers <klenaers@gmail.com>
2023-01-07 16:03:57 +0100

    energy_max_demand_import_last_13_mmonths

--- esphome/components/dsmr/text_sensor.py
+++ esphome/components/dsmr/text_sensor.py
@@ -22,7 +22,7 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Optional("water_equipment_id"): text_sensor.text_sensor_schema(),
         cv.Optional("sub_equipment_id"): text_sensor.text_sensor_schema(),
         cv.Optional("gas_delivered_text"): text_sensor.text_sensor_schema(),
-        cv.Optional("energy_import_last_13_mmonths"): text_sensor.text_sensor_schema(),
+        cv.Optional("energy_max_demand_import_last_13_mmonths"): text_sensor.text_sensor_schema(),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
